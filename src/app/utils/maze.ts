dccb582018b294ff94642337791003a33bd84775
Rory <rory.magee2@gmail.com>
2020-09-02 14:00:54 +0100

    small changes

--- src/app/utils/maze.ts
+++ src/app/utils/maze.ts
@@ -31,10 +31,9 @@ async function recursiveBacktrack(currentNode, stack, grid) {
 }
 
 function travel(currentNode:Node, nextNode:Node, grid:Node[][]) {
-    //currentNode.nodeType = NodeTypes.Default;
     nextNode.nodeType = NodeTypes.Default;
-    let y = currentNode.y;
     let x = currentNode.x;
+    let y = currentNode.y;
     if(x !== nextNode.x) {
         x = Math.min(x,nextNode.x) + 1;
     } else {
